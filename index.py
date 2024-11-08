1616ba06fea2ebdd4953d5b32e793849eb1116a3
Slava14tuv <aldarlamazhyk@yandex.ru>
2024-11-08 20:37:05 +0700

    change code

--- index.py
+++ index.py
@@ -4,9 +4,9 @@ import psycopg2
 
 app = Flask(__name__)
 
-dsn = "postgresql://postgres:admin@localhost:5432/DB2"
-conn = psycopg2.connect(dsn, options="-c client_encoding=UTF8")
-conn.autocommit = True
+# dsn = "postgresql://postgres:admin@localhost:5432/DB2"
+# conn = psycopg2.connect(dsn, options="-c client_encoding=UTF8")
+# conn.autocommit = True
 
 
 @app.route('/')
@@ -23,16 +23,16 @@ def submit():
 
 
     
-    cur = conn.cursor()
+    # cur = conn.cursor()
 
-    sql_insert_query = """
-    INSERT INTO public.users (name, email, tel) 
-    VALUES (%s, %s, %s)
-    """
-    data_to_insert = (request.form['name'], request.form['email'], request.form['tel'])
+    # sql_insert_query = """
+    # INSERT INTO public.users (name, email, tel) 
+    # VALUES (%s, %s, %s)
+    # """
+    # data_to_insert = (request.form['name'], request.form['email'], request.form['tel'])
 
 
-    cur.execute(sql_insert_query, data_to_insert)
+    # cur.execute(sql_insert_query, data_to_insert)
     
     return redirect(url_for('index'))
 
